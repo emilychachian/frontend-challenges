d7ccc677467d91022fdf95fe087e3c36103c93ef
emily chachian <emilychachian@hotmail.com>
2021-01-08 19:22:09 -0300

    Creating ProductDetails page

---
+++ app/src/pages/ProductDetails/ProductDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductDetails from "../../components/ProductDetails/ProductDetails";
+
+function ProductDetailsPage (props) {
+  return(
+    <ProductDetails />
+  )
+}
+
+export default ProductDetailsPage; 
