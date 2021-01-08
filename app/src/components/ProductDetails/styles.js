2ef692ce71543a81704fb35ed5e1d5dd6e06fa75
emily chachian <emilychachian@hotmail.com>
2021-01-08 19:22:39 -0300

    Adding ProductDetails component style

---
+++ app/src/components/ProductDetails/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Title = styled.p`
+  font-size: 21px;
+  line-height: 20px;
+  color: #333333;
+  padding: 33px 20px 20px 12px;
+  cursor: pointer;
+`;
