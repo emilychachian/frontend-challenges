d2ef090a1f8334455833ed0bfd323fe1a423774a
emily chachian <emilychachian@hotmail.com>
2021-01-08 18:21:14 -0300

    Creating price formatter

--- app/src/utils/priceFormatter.js
+++ app/src/utils/priceFormatter.js
@@ -1,11 +1,3 @@
-/* export default function priceFormatter(price) {
-    return new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    }).format(price);
-}
-*/ 
-
 export default function priceFormatter(price) {
     price = price.toString().split(''); 
     price.splice(-2, 0, ',')
