4208c4a5efbd4b97027c2699aa0e324bf17e5ee5
emily chachian <emilychachian@hotmail.com>
2021-01-08 18:21:59 -0300

    Creating ProductCard component

---
+++ app/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import priceFormatter from '../../utils/priceFormatter';
+import {
+    Container,
+    ImageContainer,
+    ProductContainer,
+    Name,
+    Price,
+    Image,
+    PriceContainer,
+    PromotionalPrice,
+    Strike,
+  } from "./styles";
+import Placeholder from "../../assets/image-placeholder.png";
+
+export default function ProductCard({ id, name, imageUrl, salePrice, promotionalPrice, quantity }) {
+    const history = useHistory(); 
+    
+    return(
+        <Container onClick={() => history.push(`/product/${id}`)}>
+            <ImageContainer>
+                <Image src={imageUrl ? imageUrl : Placeholder} />
+            </ImageContainer>
+
+            <ProductContainer>
+                <Name>{name}</Name>
+
+            <PriceContainer>
+                {promotionalPrice &&
+                promotionalPrice < salePrice ? (
+                    <PromotionalPrice>
+                        {quantity} x R${" "}
+                    <Strike>
+                        {priceFormatter(salePrice)}
+                    </Strike>{" "}
+                        por R$ {priceFormatter(promotionalPrice)}
+                    </ PromotionalPrice>
+                ) : (
+                    <Price>{quantity} X R$ {priceFormatter(salePrice)}</Price>
+             )}
+
+            </PriceContainer>
+            </ProductContainer>
+        </Container>
+    );
+};
---
+++ app/src/components/ProductCard/styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+`;
+
+export const ImageContainer = styled.div`
+  cursor: pointer;
+  width: 20%;
+`;
+
+export const ProductContainer = styled.div`
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0 auto;
+  border-bottom: 1px solid #DFE3E8;
+  `;
+
+export const Name = styled.p`
+  cursor: pointer;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; 
+`;
+
+export const Image = styled.img`
+  width: 80%;
+  height: 70%;
+  border: 1px solid #DFE4E8;
+  display: flex;
+  margin: 18% 0 10% 11px;
+`;
+
+export const Price = styled.span`
+  margin-top: 8px;
+  color: #212B36;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20px;
+`;
+
+export const PriceContainer = styled.div`
+  cursor: pointer;
+`;
+
+export const PromotionalPrice = styled.span`
+  margin-top: 8px;
+  color: #212B36;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20px;
+`;
+
+export const Strike = styled.span`
+  text-decoration: line-through;
+  color: #637381;
+`
