125e38a0377b5e05e1ebe8ddb7fdae4a3ffa48fe
emily chachian <emilychachian@hotmail.com>
2021-01-16 22:03:10 -0300

    Editing homepage

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import Home from '../../components/Home/Home'
+import { useQuery, gql } from '@apollo/client';
+import { Title } from './styles'
+import priceFormatter from '../../utils/priceFormatter';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
-function HomePage (props) {
-    return <Home />
-}
+const GET_PRODUCTS = gql`
+  query {
+    allSkus {
+      id
+      name
+      ean
+      category
+      imageUrl
+      description
+      salePrice
+      promotionalPrice
+      quantity
+    }
+  }
+`;
 
-export default HomePage; 
+ function Home() {
+
+const handleProducts = (data) => {
+  const response = [];
+   for (const product of data) {
+    const price = product.salePrice;
+    const promotionalPrice = product.promotionalPrice;
+
+    const [formattedSalePrice] = priceFormatter(price);
+    const [formattedPromotionalPrice] = priceFormatter(promotionalPrice);
+    const parsedProduct = {
+      ...product,
+      price,
+      promotionalPrice,
+    }; 
+
+    response.push(<ProductCard key={product.id} {...parsedProduct} />);
+   }
+  return response;
+};
+
+const { loading, data } = useQuery(GET_PRODUCTS);
+  return (
+    <div>
+      <Title>Produtos</Title>
+      {loading ? "Loading..." : handleProducts(data.allSkus)}
+    </div>
+  );
+};
+
+export default Home;
+
---
+++ app/src/pages/Home/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'; 
+
+export const Title = styled.div`
+    font-size: 21px;
+    line-height: 24px;
+    width: 288px;
+    padding-left: 12px; 
+    padding-top: 12px; 
+    color: #333333;
+
+`;
