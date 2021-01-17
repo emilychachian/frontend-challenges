f0383e71a8dc6ded9a77121672bd8a67c886c5e2
emily chachian <emilychachian@hotmail.com>
2021-01-16 22:14:44 -0300

    Refactoring routing

--- app/src/App.js
+++ app/src/App.js
@@ -2,24 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import api from './services/api';
-import "./App.css";
-import Home from "./components/Home/Home";
-import ProductDetailCard from './components/ProductDetailsCard/ProductDetailsCard'; 
-import ProductDetails from "./components/ProductDetails/ProductDetails";
+import Home from "./pages/Home/Home";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
 function App() { 
   return (
   <ApolloProvider client={api}>
      <Router>
-     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/product/:id">
-        <ProductDetails />
-      </Route>
-
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/product/:productId" component={ProductDetails} />
+      </Switch>
     </Router>
   </ApolloProvider>
   )  
